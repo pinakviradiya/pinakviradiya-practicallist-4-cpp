31d8d2363da5d72541031462c6f664b1eb6abce0
pinakviradiya <139138055+pinakviradiya@users.noreply.github.com>
2023-11-06 15:01:12 +0530

    PRO10.cpp

--- PRO10.cpp
+++ PRO10.cpp
@@ -15,7 +15,8 @@ class information
 int main()
 {
 	information i;
-	
+	cout<< "Viradiya pinak";
+        cout<< "220130318014";
 	cout<<"Age of given Information is :: "<<i.age<<" Years"<<endl;
 	
 	cout<<"Birth-date of given Information is :: "<<i.bdate<<endl;
